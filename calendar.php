75c608929d6fe20d9c0ad085f265cb169e4ffab6
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-08-23 10:21:36 -0700

    #13 Changed Calendar page aspect ratio

--- calendar.php
+++ calendar.php
@@ -27,7 +27,7 @@
             <div class="row">
                 <div class="container">
                     <div class="col-xs-12">
-                        <div class="embed-responsive embed-responsive-16by9">
+                        <div class="embed-responsive embed-responsive-4by3">
                             <iframe class="embed-responsive-item" src="https://www.google.com/calendar/embed?showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=uioi5lkseqrvd0f7ecrvnh91gc%40group.calendar.google.com&amp;color=%2328754E&amp;ctz=America%2FLos_Angeles" style=" border-width:0 " frameborder="0" scrolling="no"></iframe>
                         </div>
                     </div>
