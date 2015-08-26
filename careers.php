0430326a28bf82998fef9d8f535a543f647e4e6a
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-08-25 22:45:55 -0700

    #16 Moved Salaries to <small> tags on left side of page

--- careers.php
+++ careers.php
@@ -40,32 +40,23 @@
                     <div class="col-sm-8 col-xs-12">
                         <h1>Careers</h1>
                         <p>Game Development is now being seen as a viable career choice. There are many kinds of jobs in the industry.</p>
-                        <h2>Business and Management</h2>
+                        <h2>Business and Management <small>$101,572</small></h2>
                         <p>These are the people who run the video game company. Most of them do not usually work directly on the game, but they are a crucial part of making sure the game gets out there and sells.</p>
-                        <h2>Audio Professionals</h2>
+                        <h2>Audio Professionals <small>$95,682</small></h2>
                         <p>Audio Professionals create everything you hear in a video game. They create voices for characters, sound effects, and music.</p>
-                        <h2>Programmers</h2>
+                        <h2>Programmers <small>$93,251</small></h2>
                         <p>Programmers write the code that makes the game come together. They work with the designers to make their vision come to life. Programmers are usually specialized, working in one field specifically i.e. A.I. Programming.</p>
-                        <h2>Artists and Animators</h2>
+                        <h2>Artists and Animators <small>$74,349</small></h2>
                         <p>Artists create the visuals of the games. They create and animate any 3D models, or 2D spritesheets. They also create logos, box art, etc.</p>
-                        <h2>Producers</h2>
+                        <h2>Producers <small>$82,286</small></h2>
                         <p>Producers oversee the development of the game. They can either come from the publishing company, or from within the company developing the game. They are similar to a product manager.</p>
-                        <h2>Game Designers</h2>
+                        <h2>Game Designers <small>$73,864</small></h2>
                         <p>A game designer comes up with the concepts of the game. They work with the programmers, sound engineers, and the artists to make a game out of their own vision. To be a designer, many years of experience is needed.</p>
-                        <h2>Quality Assurance</h2>
+                        <h2>Quality Assurance <small>$54,833</small></h2>
                         <p>Quality Assurance test the games and report any errors. Their job is crucial to making sure the game is released with no bugs or design flaws.</p>
+                        <a href="http://www.gamasutra.com/view/news/221533/Game_Developer_Salary_Survey_2014_The_results_are_in.php" target="_blank">Salary Source</a>
                     </div>
                     <div class="col-sm-4 col-xs-12">
-                        <h1>Salaries <small><a href="http://www.gamasutra.com/view/news/221533/Game_Developer_Salary_Survey_2014_The_results_are_in.php" target="_blank">Source</a></small></h1>
-                        <ul>
-                            <li>Business and management: $101,572</li>
-                            <li>Audio professionals: $95,682</li>
-                            <li>Programmers: $93,251</li>
-                            <li>Artists and animators: $74,349</li>
-                            <li>Producers: $82,286</li>
-                            <li>Game designers: $73,864</li>
-                            <li>Quality Assurance: $54,833</li>
-                        </ul>
                         <h1>Game Companies in Southern California</h1>
                         <p>Theses are just few of many.</p>
                         <a href="http://atlus.com/" target="_blank"><img src="http://atlus.com/wp-content/uploads/2014/12/Atlus_Logo_Thin_Stroke_277x108.png" alt="Atlus" class="img-responsive" /></a>
