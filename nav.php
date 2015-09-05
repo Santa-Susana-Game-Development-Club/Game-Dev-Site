ad282b6f9ee280b4fbf71d5c39ba33488792dca8
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-09-04 18:54:04 -0700

    #27 Created Trips Page With IndieCade Information

--- nav.php
+++ nav.php
@@ -3,6 +3,7 @@
 <li><a href="/tutorials.php">Tutorials</a></li>
 <li><a href="/downloads.php">Downloads</a></li>
 <li><a href="/games.php">Games</a></li>
+<li><a href="/trips.php">Trips</a></li>
 <li><a href="/signup.php">Sign Up</a></li>
 <li><a href="/careers.php">Careers</a></li>
 <li><a href="/calendar.php">Calendar</a></li>
---
+++ trips.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Field Trips and Fundraisers | Santa Susana Game Development Club</title>
+    <link href="css/main.css" rel="stylesheet" />
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+    
+    <?php 
+    
+        include('header.php');
+
+    ?>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="container">
+                    <div class="col-sm-9 col-xs-12">
+                        <h1>Field Trips and Fundraisers</h1>
+                        <p>
+                            Game Development Club is planning to have field trips and fundraisers this year and in years to come.
+                        </p>
+                        <h2>IndieCade Festival 2015</h2>
+                        <img src="/images/content/trips/indiecade.png" class="img-responsive" onclick="window.open('http://www.indiecade.com/2015');" style="cursor: pointer;"/>
+                        <h3>About</h3>
+                        <p>
+                            The IndieCade Festival is the biggest event dedicated to celebrating independent games in the nation. This premiere event is held each October in the Los Angeles area and is open to the industry and to the public.
+                        </p>
+                        <h3>When is the Event</h3>
+                        <p>
+                            The festival is from Thursday, October 22<sup>nd</sup> to Sunday, October 25<sup>th</sup>. The club plans on going on Saturday, October 24<sup>th</sup>.
+                        </p>
+                        <p>
+                            More details will be available soon.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <?php 
+    
+        include('footer.php');
+
+    ?>
+
+    <script src="/js/jquery.1.11.2.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</body>
+</html>
