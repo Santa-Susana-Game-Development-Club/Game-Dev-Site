b5dc694be4ae190892a7cbdfe85bd13c72c6db0d
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-10-04 10:37:49 -0700

    #42 Footer Test

--- inc/footer.php
+++ inc/footer.php
@@ -1,18 +1,16 @@
-<?php 
-echo '
+
 <footer>
         <div id="ftr-wrapper">
             <div class="container">
                 <div class="row">
                     <div class="col-xs-12" id="ftr-links">
-                        <ul>';
-    
+                        <ul>
+                                <?php
                                 include('/inc/nav.php');
-echo '
+                                ?>
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-    </footer>';
-?>
+    </footer>
