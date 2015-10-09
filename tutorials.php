469e30dbfa4b5446a91879314e7b78793b729cd2
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-10-08 20:22:00 -0700

    #46 Head Include

--- tutorials.php
+++ tutorials.php
@@ -1,17 +1,12 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/inc/head.php";
+    include_once($path);
+    ?>
     <title>Tutorials | Santa Susana Game Development Club</title>
-    <link href="css/main.css" rel="stylesheet" />
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
     <style>
         #pl2, #pl3, #pl4, #pl5 {
             display: none;
