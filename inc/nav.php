046fc79bee05e3c4e84e57833514ba70a9cdd7de
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-10-10 13:49:12 -0700

    #55 Added YouTube Link to nav

--- inc/nav.php
+++ inc/nav.php
@@ -8,5 +8,6 @@ echo '
 <li><a href="/trips.php">Trips</a></li>
 <li><a href="/signup.php">Sign Up</a></li>
 <li><a href="/careers.php">Careers</a></li>
-<li><a href="/calendar.php">Calendar</a></li>';
+<li><a href="/calendar.php">Calendar</a></li>
+<li><a href="https://www.youtube.com/user/sshsgamedev" target="_blank"><i class="fa fa-youtube" id="youtubeLink"></i></a></li>';
 ?>
