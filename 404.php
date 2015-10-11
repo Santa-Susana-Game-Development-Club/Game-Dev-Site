37dd75ec13630c6423cb1d1b79d46f3d1139068b
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2015-10-10 18:57:13 -0700

    #52 Started 404 page

---
+++ 404.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <?php
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/inc/head.php";
+    include_once($path);
+    ?>
+    <title>Santa Susana Game Development Club</title>
+</head>
+<body>
+    
+    <?php 
+    
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/inc/header.php";
+    include_once($path);
+
+    ?>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="container">
+                    <div class="col-xs-12">
+                        <h1>404 - Not Found</h1>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <?php 
+    
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/inc/footer.php";
+    include_once($path);
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= "/inc/javascript.php";
+    include_once($path);
+
+    ?>
+</body>
+</html>
