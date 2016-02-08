b84e165145d078581e5e3039091218bd8814a777
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2016-02-07 17:38:35 -0800

    Improved Tutorials Page

--- tutorials.php
+++ tutorials.php
@@ -37,42 +37,17 @@
             <div class="row">
                 <div class="container">
                     <div class="col-xs-12">
-                        <ul class="nav nav-tabs nav-justified">
-                          <li role="presentation" class="active" id="t1" onclick="showPlaylist('#t1', '#pl1');"><a class="tab-link">Full Programming Tutorial</a></li>
-                          <li role="presentation" id="t2" onclick="showPlaylist('#t2', '#pl2');"><a class="tab-link">Intro To Programming</a></li>
-                          <li role="presentation" id="t3" onclick="showPlaylist('#t3', '#pl3');"><a class="tab-link">Pong</a></li>
-                          <li role="presentation" id="t4" onclick="showPlaylist('#t4', '#pl4');"><a class="tab-link">Space Invaders</a></li>
-                          <li role="presentation" id="t5" onclick="showPlaylist('#t5', '#pl5');"><a class="tab-link">Intermediate Java</a></li>
-                          <li role="presentation" id="t6" onclick="showPlaylist('#t6', '#pl6');"><a class="tab-link">Code Lapse</a></li>
+                        <ul class="nav nav-tabs nav-justified" id="navTabs">
+                          <li role="presentation" class="active" id="t1"><a class="tab-link">Full Programming Tutorial</a></li>
+                          <li role="presentation" id="t2"><a class="tab-link">Intro To Programming</a></li>
+                          <li role="presentation" id="t3"><a class="tab-link">Pong</a></li>
+                          <li role="presentation" id="t4"><a class="tab-link">Space Invaders</a></li>
+                          <li role="presentation" id="t5"><a class="tab-link">Intermediate Java</a></li>
+                          <li role="presentation" id="t6"><a class="tab-link">Code Lapse</a></li>
                         </ul>
-                        <section id="pl1">
+                        <section>
                             <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYkiqqE0fTuOGg0BLX920aMs7" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                        </section>
-                        <section id="pl2">
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYkjq3wsxyHQ-UJvrLAi1XjYV" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                        </section>
-                        <section id="pl3">
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYkhkXBTuTyBkfK10P3fpsRkJ" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                        </section>
-                        <section id="pl4">
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYki0ybyvz-DpxL-85q5yaQWz" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                        </section>
-                        <section id="pl5">
-                          <div class="embed-responsive embed-responsive-16by9">
-                            <iframe src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYkihuquwykpJQcMfdp8kjy3l" frameborder="0" class="embed-responsive-item" allowfullscreen></iframe>
-                          </div>
-                        </section>
-                        <section id="pl6">
-                            <div class="embed-responsive embed-responsive-16by9">
-                                <iframe class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYkhaknyJbGT7EzZLelxHlOfw" frameborder="0" allowfullscreen></iframe>
+                                <iframe  id="playList" class="embed-responsive-item" src="//www.youtube.com/embed/videoseries?list=PLXGqUKNYJYkiqqE0fTuOGg0BLX920aMs7" frameborder="0" allowfullscreen></iframe>
                             </div>
                         </section>
                     </div>
@@ -91,21 +66,6 @@
     include_once($path);
 
     ?>
-    <script type="text/javascript">
-      $(document).ready(function() {
-        if($(location).attr('search') != "") {
-        var hashNumber = parseInt($(location).attr('search').replace("?pl=", ''));
-        if(hashNumber >= 1 && hashNumber <= 6) {
-          showPlaylist("#t" + hashNumber, "#pl" + hashNumber);
-        }
-      }
-      });
-      function showPlaylist(navid, pid) {
-        $("#t1, #t2, #t3, #t4, #t5, #t6").removeClass("active");
-        $(navid).addClass("active");
-        $("#pl1, #pl2, #pl3, #pl4, #pl5, #pl6").hide();
-        $(pid).show();
-      }
-    </script>
+    <script src="tutorials.js"></script>
 </body>
 </html>
